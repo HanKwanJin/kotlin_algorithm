40561320d35a255154c0cdd8d46e7b357b34bd80
DoooongDong <rhkswls98@kakao.com>
2022-02-17 17:21:14 +0900

    약수의개수와덧셈

---
+++ 프로그래머스/약수의개수와덧셈.kt
@@ -0,0 +1,18 @@
+fun solution3(left: Int, right: Int): Int {
+    var answer: Int = 0
+
+    for(i in left..right){
+        var cnt = 0
+        for(j in 1..i){
+            if(i % j == 0){
+                cnt++
+            }
+        }
+        if(cnt % 2 == 0) answer += i
+        else answer -= i
+    }
+    return answer
+}
+fun main(){
+    print( solution(13,17))
+}
