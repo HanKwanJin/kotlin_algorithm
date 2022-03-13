5b5c58b1cd8e9b99047c091f03edb02b3da85694
DoooongDong <rhkswls98@kakao.com>
2022-03-13 23:36:48 +0900

    조이스틱

--- 프로그래머스/조이스틱.kt
+++ 프로그래머스/조이스틱.kt
@@ -3,20 +3,16 @@ import kotlin.math.min
 class 조이스틱 {
     fun solution(name: String): Int {
         var answer = 0
+        var next: Int
         var move = name.length - 1
         for (i in name.indices){
-            var next = i + 1
+            answer += min(name[i] - 'A', 'Z' - name[i] + 1)
+            next = i + 1
             while (next < name.length && name[next] == 'A'){
                 next++
             }
-            move = min(move, i+name.length-next + i)
-            if (name[i] != 'A'){
-                var tmp = name[i].toInt() - 65
-                if (tmp >= 13) {
-                    tmp = 26-tmp
-                }
-                answer += tmp
-            }
+            move = min(move, i*2 + name.length - next)
+            move = min(move, (name.length - next) * 2 + i)
         }
         return answer + move
     }
